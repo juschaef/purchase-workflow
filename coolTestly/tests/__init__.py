e377cab4a731f45bda977f204ae23ac9760bdfe2
ju <julien.schaeffer@gmail.com>
2015-11-04 14:08:53 +0100

    linter ok

--- coolTestly/tests/__init__.py
+++ coolTestly/tests/__init__.py
@@ -1,2 +1,1 @@
 from openerp import tests
-
--- coolTestly/tests/tests.py
+++ coolTestly/tests/tests.py
@@ -1,10 +1,6 @@
-from mock import Mock
-
 import openerp.tests.common as test_common
-from openerp.osv.orm import browse_record
-
 
 
 class TestFailed(test_common.TransactionCase):
-	def test_failded(self):
-		self.assertEqual('42', '41')
+    def test_failded(self):
+        self.assertEqual('42', '41')
